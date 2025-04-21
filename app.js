345bfd282ffad17e9137df8f472173a183fb6da3
Tsimmuaj <tyang4204@gmail.com>
2025-04-21 10:12:54 -0500

    Suggest a song through link in working condition

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 5000;
 const path = require('path');
 const bodyParser = require('body-parser');
 
@@ -13,17 +13,26 @@ const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
 });
 
+app.use(express.static(path.join(__dirname)));
+
+process.on('unhandledRejection', err => {
+    console.error('Unhandled promise rejection:', err);
+});
+
+process.on('uncaughtException', err => {
+    console.error('Uncaught exception:', err);
+});
+
 
 app.get('/callback', async (req, res) => {
     const code = req.query.code;
-    console.log("Access token:", data.body.access_token);
 
     try {
         const data = await spotifyApi.authorizationCodeGrant(code);
         spotifyApi.setAccessToken(data.body.access_token);
         spotifyApi.setRefreshToken(data.body.refresh_token);
 
-        res.send('Logged in! You can now add songs via the API');
+        res.redirect('/');
     } catch (err) {
         console.error(err);
         res.send('Something went wrong');
@@ -31,7 +40,7 @@ app.get('/callback', async (req, res) => {
 });
 
 
-app.listen(port, () => {
+app.listen(port, '127.0.0.1', () => {
     console.log(`Listening at http://127.0.0.1:${port}`);
 });
 
@@ -51,18 +60,37 @@ app.get('/form', (req, res) => {
 
 
 app.post('/submit-song', (req, res) => {
-    const trackUri = req.body.trackUri;
+    let input = req.body.trackUri.trim();
     const playlistId = '1sTzjueAClM4LRulUso1pV';
 
+    let trackUri;
+    if (input.includes('open.spotify.com/track/')) {
+        const match = input.match(/track\/([a-zA-Z0-9]{22})/);
+        if (match) {
+            trackUri = `spotify:track:${match[1]}`;
+        } else {
+            return res.send('Invalid Spotify track URL');
+        }
+    }else if (input.startsWith('spotify:track')) {
+        trackUri = input;
+    } else {
+        return res.send('Please enter a valid Spotify track URI or link.');
+    }
     spotifyApi.addTracksToPlaylist(playlistId, [trackUri])
         .then(() => {
-            res.send(`<p> Song added to playlist!</p><a href="/form">Back</a>`);
+            res.send(`<p> Song added to playlist!</p><a href="/">Back</a>`);
+        })
+        .catch(err => {
+            console.error(err);
+            res.send(`<p> Failed to add song. ${err.body?.error?.message || ''}</p><a href="/">Back</a>`);
         });
 });
+
 
 app.get('/login', (req, res) => {
     const scopes = ['playlist-modify-public', 'playlist-modify-private'];
     const authUrl = spotifyApi.createAuthorizeURL(scopes);
+    console.log('>> Redirecting user to Spotify:', authUrl);
     res.redirect(authUrl);
 });
 
