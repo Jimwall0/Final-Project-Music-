9ddbb8e4368548eb4bbc63f62a6f48c63137eaff
Tsimmuaj <tyang4204@gmail.com>
2025-04-21 18:44:20 -0500

    moved styles to style.js

---
+++ styles.js
@@ -0,0 +1,12 @@
+function changeTheme(themeFile) {
+    // get the stylesheet link element
+    // change the href attribute to the new theme file
+    const stylesheet = document.getElementById("themechange");
+    stylesheet.href = themeFile;
+}
+
+fetch('form.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById('recommend').insertAdjacentHTML('afterbegin', data);
+})
