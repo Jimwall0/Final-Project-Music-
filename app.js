0e69db525f7dc89ed6ec6b84cbd11c9589b37be8
Tsimmuaj <tyang4204@gmail.com>
2025-04-22 14:56:51 -0500

    added callback route

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ app.get('/login', (req, res) => {
             client_id: client_id,
             scope: scope,
             redirect_uri: redirect_uri,
-            state: state,
+            state: state
         }));
+});
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+
+    if (state === null) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+    }));
+    } else {
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'content-type': 'spplication/x-www-form-urlencoded',
+                'Authorization': `Basic ${(new Buffer.from(client_id + ':' + client_secret).tostring('base64'))}`
+            },
+            json: true
+        };
+    }
 });
